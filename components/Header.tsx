59b12208f0f5fe6f108f6355f0d64d947949be3c
John Canero <johncanero.official@gmail.com>
2023-05-02 11:05:58 +0800

    Customize Navbar Colors

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,24 +4,17 @@ import Image from 'next/image'
 
 function Header() {
     return (
-        <nav>
-            <div className='flex justify-between'>
+        <nav className="bg-gradient-to-r from-white from-40% to-yellow-500">
+            <div className="container flex items-center justify-between p-12 mx-auto">
                 {/* Likha Design Agency - Brand Logo & Name */}
                 <div className='flex'>
-                    <Image
-                        src=""
-                        alt="Likha Design Agency Brand Logo"
-                        width={100}
-                        height={100}
-                    ></Image>
-                    <p>Likha</p>
+                    <p>Likha Design Agency</p>
                 </div>
 
                 <div className='flex'>
                     <Link href="/" rel="noopener noreferrer" passHref>About</Link>
                     <Link href="/" rel="noopener noreferrer" passHref>Service</Link>
                     <Link href="/" rel="noopener noreferrer" passHref>Projects</Link>
-                    {/* Schedule A Call Button */}
                     <Link href="/schedule-call">
                         <button className="px-4 py-2 font-bold text-yellow-600 rounded bg-stone-900 hover:bg-stone-700">
                             Schedule A Call
