63d9543ec904668ca40b218978a10edfbe1b4de1
John Canero <johncanero.official@gmail.com>
2023-05-02 12:43:53 +0800

    Add Navbar Dropdown File for Mobile and Add Comments

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,29 +3,34 @@ import Image from 'next/image'
 
 function Header() {
     return (
-        <nav className='lg:px-80 lg:py-14'>
-            <div className="container flex items-center justify-between mx-auto">
-                {/* Likha Desig nAgency - Brand Logo & Name */}
-                <div className='flex'>
-                    <Link href="/" rel="noopener noreferrer" >
-                        <p className='text-3xl font-bold'>Likha</p>
-                    </Link>
-                </div>
+        <div>
+            {/* Desktop and Tablet */}
+            <nav className='lg:px-80 lg:py-14'>
+                <div className="container flex items-center justify-between mx-auto">
+                    {/* Likha Desig nAgency - Brand Logo & Name */}
+                    <div className='flex'>
+                        <Link href="/" rel="noopener noreferrer" >
+                            <p className='text-3xl font-bold'>Likha</p>
+                        </Link>
+                    </div>
 
-                <div className='flex gap-12'>
-                    <Link href="/about" rel="noopener noreferrer" className='mt-4 font-medium hover:text-stone-700' passHref>About</Link>
-                    <Link href="/service" rel="noopener noreferrer" className='mt-4 font-medium hover:text-stone-700' passHref>Service</Link>
-                    <Link href="/projects" rel="noopener noreferrer" className='mt-4 font-medium hover:text-stone-700' passHref>Projects</Link>
-                    <Link href="/schedule-call">
-                        <button className="px-8 py-5 font-bold rounded-xl text-amber-50 bg-stone-900 hover:bg-stone-700">
-                            Schedule A Call
-                        </button>
-                    </Link>
-                    {/* Dark Mode Button */}
+                    <div className='flex gap-12'>
+                        <Link href="/about" rel="noopener noreferrer" className='mt-4 font-medium hover:text-stone-700' passHref>About</Link>
+                        <Link href="/service" rel="noopener noreferrer" className='mt-4 font-medium hover:text-stone-700' passHref>Service</Link>
+                        <Link href="/projects" rel="noopener noreferrer" className='mt-4 font-medium hover:text-stone-700' passHref>Projects</Link>
+                        <Link href="/schedule-call">
+                            <button className="px-8 py-5 font-bold rounded-xl text-amber-50 bg-stone-900 hover:bg-stone-700">
+                                Schedule A Call
+                            </button>
+                        </Link>
+                        {/* Dark Mode Button */}
+                    </div>
                 </div>
-            </div>
-            {/* Integrate NextAuth Sign In / Login / */}
-        </nav>
+                {/* Integrate NextAuth Sign In / Login / */}
+            </nav>
+
+            {/* Mobile */}
+        </div>
     )
 }
 
---
+++ components/NavbarDropdown.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NavbarDropdown() {
+  return (
+    <div className='md:hidden'>
+      
+    </div>
+  )
+}
+
+export default NavbarDropdown
