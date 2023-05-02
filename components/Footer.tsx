9b39555bdd9a0d8a19182f12db3fb2db27924f78
John Canero <johncanero.official@gmail.com>
2023-05-02 09:13:54 +0800

    Add Header/Footer components, Tailwind CSS & coding guides for clarity.

---
+++ components/Footer.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            <div className='p-6 text-center'>
+                Footer
+            </div>
+
+            {/* Coding Guide */}
+            {/* Add "Let's build something great together" */}
+            {/* Schedule A Call */}
+
+            {/* Footer Copyright: John Cañero (Linking to my Website: https://johncanero.com/)*/}
+        </div>
+    )
+}
+
+export default Footer
---
+++ components/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            <div className='p-6 text-center'>
+                Header
+            </div>
+            {/* Coding Guide */}
+            {/* Add Logo of Likha Design Agency */}
+            {/* Add About Link Page of Likha Design Agency */}
+            {/* Add Service Link Page of Likha Design Agency */}
+            {/* Add Projects Link Page of Likha Design Agency */}
+            {/* Add Schedule A Call Button from Calendly of Likha Design Agency */}
+
+            {/* Integrate NextAuth Sign In / Login / */}
+        </div>
+    )
+}
+
+export default Header
