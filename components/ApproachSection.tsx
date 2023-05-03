3c78b4972c77362460018cd557dd94e6f1bfcc9d
John Canero <johncanero.official@gmail.com>
2023-05-04 02:16:50 +0800

    Adjust Content for Approach Section

--- components/ApproachSection.tsx
+++ components/ApproachSection.tsx
@@ -3,10 +3,10 @@ function ApproachSection() {
         <div>
             <div className='flex justify-between'>
                 <div>
-                    <h2 className='relative z-10 font-bold lg:text-6xl lg:-right-72 lg:mt-36 lg:pr-48'>Our approach for creating a winning brand</h2>
+                    <h2 className='relative z-10 font-bold lg:text-6xl lg:-right-72 lg:mt-96 lg:pr-48'>Our approach for creating a winning brand</h2>
                 </div>
 
-                <div className='relative md:w-10/12 bg-amber-300 lg:pt-60 lg:pb-60 lg:px-28'>
+                <div className='relative md:w-10/12 bg-amber-300 lg:pt-40 lg:pb-40 lg:px-28'>
                     <div className='lg:px-32'>
                         {/* Brand Strategy */}
                         <div>
